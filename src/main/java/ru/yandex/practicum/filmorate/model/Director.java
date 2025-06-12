b8a87900036879024da2fe8beaf92d95a7d21c27
Irina Kuznetsova <dyachenkoia@yandex.ru>
2025-06-12 14:37:02 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.annotations.Marker;
+
+@Data
+@AllArgsConstructor
+public class Director {
+    @Positive(groups = Marker.OnUpdate.class, message = "id должен быть указан и быть больше нуля")
+    private int id;
+    private String name;
+}
