a17e73dd1ed96319a02a386b891324d2e7b7394f
chuvi89 <chuvi.men@gmail.com>
2025-06-14 23:01:18 +0300

    review functional

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.filmorate.annotations.Marker;
 
 
 @Data
@@ -18,5 +16,4 @@ public class Review {
     private Long userId;
     private Long filmId;
     private long useful = 0;
-
 }
