5a6e0ee1708a2798c97ac6cfcfd848a1279c9fc6
chuvi89 <chuvi.men@gmail.com>
2025-06-18 10:58:19 +0300

    add tests

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -32,13 +32,19 @@ public class ReviewService {
     }
 
     public Collection<Review> findReviews(Long filmId, int count) {
-        try {
-            filmStorage.getById(filmId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Фильм не найден");
+        Collection<Review> reviews;
+        if (filmId == null) {
+            reviews = reviewStorage.findReviews(filmId, count);
+        } else {
+            try {
+                filmStorage.getById(filmId);
+                reviews = reviewStorage.findReviews(filmId, count);
+            } catch (NotFoundException e) {
+                throw new NotFoundException("Фильм не найден");
+            }
         }
 
-        return reviewStorage.findReviews(filmId, count);
+        return reviews;
     }
 
     public Review findById(long id) {
@@ -52,6 +58,8 @@ public class ReviewService {
                 .orElseThrow(() -> new ValidationException("Film ID не может быть null"));
         Optional.ofNullable(review.getIsPositive())
                 .orElseThrow(() -> new ValidationException("IsPositive не может быть null"));
+        Optional.ofNullable(review.getContent())
+                .orElseThrow(() -> new ValidationException("Content не может быть null"));
 
         try {
             filmStorage.getById(review.getFilmId());
@@ -64,88 +72,68 @@ public class ReviewService {
             throw new NotFoundException("Пользователель не найден");
         }
         Review createdReview = reviewStorage.save(review);
-        long eventId = userEventFeedDbStorage.addEventReviewAdd(createdReview.getUserId(), createdReview.getReviewId());
+        userEventFeedDbStorage.addEventReviewAdd(createdReview.getUserId(), createdReview.getReviewId());
         return createdReview;
     }
 
     public Review update(Review review) {
-        try {
-            filmStorage.getById(review.getFilmId());
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Фильм не найден");
-        }
-        try {
-            userStorage.getById(review.getUserId());
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователь не найден");
-        }
+        Optional.ofNullable(review.getUserId())
+                .orElseThrow(() -> new ValidationException("User ID не может быть null"));
+        Optional.ofNullable(review.getFilmId())
+                .orElseThrow(() -> new ValidationException("Film ID не может быть null"));
+        Optional.ofNullable(review.getIsPositive())
+                .orElseThrow(() -> new ValidationException("IsPositive не может быть null"));
+        Optional.ofNullable(review.getContent())
+                .orElseThrow(() -> new ValidationException("Content не может быть null"));
+
+        Optional.ofNullable(filmStorage.getById(review.getFilmId()))
+                .orElseThrow(() -> new NotFoundException("Фильм не найден"));
+        Optional.ofNullable(userStorage.getById(review.getUserId()))
+                .orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+
         Review updatedReview = reviewStorage.saveUpdatedObject(review);
-        long eventId = userEventFeedDbStorage.addEventReviewUpdate(updatedReview.getUserId(), updatedReview.getReviewId());
+        userEventFeedDbStorage.addEventReviewUpdate(updatedReview.getUserId(), updatedReview.getReviewId());
         return updatedReview;
     }
 
     public void deleteReview(long reviewId) {
         Review review = findById(reviewId);
-        long eventId = userEventFeedDbStorage.addEventReviewRemove(review.getUserId(), reviewId);
+        userEventFeedDbStorage.addEventReviewRemove(review.getUserId(), reviewId);
         reviewStorage.remove(reviewId);
     }
 
     public void addLike(long reviewId, long userId) {
-        try {
-            reviewStorage.getById(reviewId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Отзыв не найден");
-        }
-        try {
-            userStorage.getById(userId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователель не найден");
-        }
+        Optional.ofNullable(reviewStorage.getById(reviewId))
+                .orElseThrow(() -> new NotFoundException("Отзыв не найден"));
+        Optional.ofNullable(userStorage.getById(userId))
+                .orElseThrow(() -> new NotFoundException("Пользователель не найден"));
 
         reviewStorage.saveLike(reviewId, userId);
     }
 
     public void addDislike(long reviewId, long userId) {
-        try {
-            reviewStorage.getById(reviewId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Отзыв не найден");
-        }
-        try {
-            userStorage.getById(userId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователель не найден");
-        }
+        Optional.ofNullable(reviewStorage.getById(reviewId))
+                .orElseThrow(() -> new NotFoundException("Отзыв не найден"));
+        Optional.ofNullable(userStorage.getById(userId))
+                .orElseThrow(() -> new NotFoundException("Пользователель не найден"));
 
         reviewStorage.saveDislike(reviewId, userId);
     }
 
     public void removeUserLike(long reviewId, long userId) {
-        try {
-            reviewStorage.getById(reviewId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Отзыв не найден");
-        }
-        try {
-            userStorage.getById(userId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователель не найден");
-        }
+        Optional.ofNullable(reviewStorage.getById(reviewId))
+                .orElseThrow(() -> new NotFoundException("Отзыв не найден"));
+        Optional.ofNullable(userStorage.getById(userId))
+                .orElseThrow(() -> new NotFoundException("Пользователель не найден"));
 
         reviewStorage.removeUserLike(reviewId, userId);
     }
 
     public void removeUserDislike(long reviewId, long userId) {
-        try {
-            reviewStorage.getById(reviewId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Отзыв не найден");
-        }
-        try {
-            userStorage.getById(userId);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("Пользователель не найден");
-        }
+        Optional.ofNullable(reviewStorage.getById(reviewId))
+                .orElseThrow(() -> new NotFoundException("Отзыв не найден"));
+        Optional.ofNullable(userStorage.getById(userId))
+                .orElseThrow(() -> new NotFoundException("Пользователель не найден"));
 
         reviewStorage.removeUserLike(reviewId, userId);
     }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users (
     id bigint primary key auto_increment,
     email varchar(150) NOT NULL UNIQUE,
@@ -7,26 +6,22 @@ CREATE TABLE IF NOT EXISTS users (
     birthday date
 );
 
-DROP TABLE IF EXISTS friends CASCADE;
 CREATE TABLE IF NOT EXISTS friends (
     user_id bigint REFERENCES users(id) ON DELETE CASCADE,
     friend_id bigint REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, friend_id)
 );
 
-DROP TABLE IF EXISTS genres CASCADE;
 CREATE TABLE IF NOT EXISTS genres (
     id integer primary key auto_increment,
     name varchar(150) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS MPA CASCADE;
 CREATE TABLE IF NOT EXISTS MPA (
     id integer primary key auto_increment,
     name varchar(40) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS films CASCADE;
 CREATE TABLE IF NOT EXISTS films (
     id bigint primary key auto_increment,
     name varchar(150) NOT NULL,
@@ -36,21 +31,18 @@ CREATE TABLE IF NOT EXISTS films (
     MPA_id integer REFERENCES MPA(id)
 );
 
-DROP TABLE IF EXISTS likes CASCADE;
 CREATE TABLE IF NOT EXISTS likes (
     film_id bigint REFERENCES films(id) ON DELETE CASCADE,
     user_id bigint REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (film_id, user_id)
 );
 
-DROP TABLE IF EXISTS film_genre CASCADE;
 CREATE TABLE IF NOT EXISTS film_genre (
     film_id bigint REFERENCES films(id) ON DELETE CASCADE,
     genre_id integer REFERENCES genres(id) ON DELETE CASCADE,
     PRIMARY KEY (film_id, genre_id)
 );
 
-DROP TABLE IF EXISTS reviews CASCADE;
 CREATE TABLE IF NOT EXISTS reviews (
     review_id bigint primary key auto_increment,
     content varchar(200) NOT NULL,
@@ -59,40 +51,34 @@ CREATE TABLE IF NOT EXISTS reviews (
     film_id bigint REFERENCES films(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS useful_tab CASCADE;
 CREATE TABLE IF NOT EXISTS useful_tab (
     review_id bigint REFERENCES reviews(review_id) ON DELETE CASCADE,
     user_id bigint REFERENCES users(id) ON DELETE CASCADE,
     useful_flag integer NOT NULL
 );
 
-DROP TABLE IF EXISTS directors CASCADE;
 CREATE TABLE IF NOT EXISTS directors (
     id bigint primary key auto_increment,
     name varchar(150) NOT NULL
 );
 
-DROP TABLE IF EXISTS film_director CASCADE;
 CREATE TABLE IF NOT EXISTS film_director (
     film_id bigint REFERENCES films(id) ON DELETE CASCADE,
     director_id bigint REFERENCES directors(id) ON DELETE CASCADE,
     PRIMARY KEY (film_id, director_id)
 );
 
-DROP TABLE IF EXISTS eventType CASCADE;
 CREATE TABLE IF NOT EXISTS eventType (
     id INTEGER primary key auto_increment,
     name varchar(40) NOT NULL UNIQUE
 );
 
-DROP TABLE IF EXISTS operationType CASCADE;
 CREATE TABLE IF NOT EXISTS operationType (
     id INTEGER primary key auto_increment,
     name varchar(40) NOT NULL UNIQUE
 );
 
 --создание таблицы "Ленты событий" пользователя
-DROP TABLE IF EXISTS userEventFeed CASCADE;
 CREATE TABLE IF NOT EXISTS userEventFeed (
     event_id BIGINT PRIMARY KEY auto_increment,
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
